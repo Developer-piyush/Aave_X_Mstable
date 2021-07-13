be61f4f606e481ac210f6cc0bf053c33db97c818
developer-piyush <developerpiyush007@gmail.com>
2021-07-13 13:28:23 +0530

    gh-pages

---
+++ precache-manifest.e83ab3929b068983183d283af8f153b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d790c03d0c4cc315dd50e7d48b97e533",
+    "url": "/Aave_X_Mstable/index.html"
+  },
+  {
+    "revision": "3f7de9fea49d390d1349",
+    "url": "/Aave_X_Mstable/static/css/2.7118e3f5.chunk.css"
+  },
+  {
+    "revision": "7440043aea1e660b4fbf",
+    "url": "/Aave_X_Mstable/static/css/main.06266460.chunk.css"
+  },
+  {
+    "revision": "3f7de9fea49d390d1349",
+    "url": "/Aave_X_Mstable/static/js/2.b8e806ff.chunk.js"
+  },
+  {
+    "revision": "306ee9dc751947a6e31dc87312b63e91",
+    "url": "/Aave_X_Mstable/static/js/2.b8e806ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7440043aea1e660b4fbf",
+    "url": "/Aave_X_Mstable/static/js/main.d4256eb3.chunk.js"
+  },
+  {
+    "revision": "1e8b136fa281f23277a8",
+    "url": "/Aave_X_Mstable/static/js/runtime-main.66b95ba8.js"
+  },
+  {
+    "revision": "f0181479ea35d984e778408808136eb6",
+    "url": "/Aave_X_Mstable/static/media/0x.f0181479.png"
+  },
+  {
+    "revision": "2dcc77d9a617cbd2a61d6fc2b9d7bc9f",
+    "url": "/Aave_X_Mstable/static/media/aave.2dcc77d9.png"
+  },
+  {
+    "revision": "b71a79b98e5354f660c64a51720832bd",
+    "url": "/Aave_X_Mstable/static/media/logo.b71a79b9.png"
+  },
+  {
+    "revision": "e9ada72b93b9cdb343c1f49bfa413c29",
+    "url": "/Aave_X_Mstable/static/media/metamask.e9ada72b.png"
+  }
+]);
